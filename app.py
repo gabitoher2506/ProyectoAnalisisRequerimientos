ba122ad3a0db504f229c095debc56a7c97429152
Noé Rojas Rodríguez <nrojas70164@ufide.ac.cr>
2024-08-13 09:08:26 -0600

    Modulo MPE
    Mantenimiento Predictivo
    Funciones Técnicas y Administrativas

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def home():
     return render_template('index.html')
 
@@ -38,9 +38,17 @@ def registro_entregas():
 def incidencias():
     return render_template('incidencias.html')
 
-@app.route('/autenticacion')
-def autentificacion():
-    return render_template('autentificacion.html')
+@app.route('/')
+def autenticacion():
+    return render_template('autenticacion.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/reset_password')
+def reset_password():
+    return render_template('reset_password.html')
 
 @app.route('/empleados')
 def empleados():
@@ -54,13 +62,61 @@ def servicios():
 def tienda():
     return render_template('tienda.html')
 
-@app.route('/ventas')
-def ventas():
-    return render_template('ventas.html')
+# Ruta para procesar la venta
+@app.route('/process_sale')
+def process_sale():
+    return render_template('ventas')
+
+# Ruta para generar reportes
+@app.route('/generate_report')
+def generate_report():
+    return render_template('ventas')
+
+# Ruta para configurar promociones
+@app.route('/configure_promotion')
+def configure_promotion():
+    return render_template('ventas')
+
+# Ruta para configurar métodos de pago
+@app.route('/configure_payment_method')
+def configure_payment_method():
+    return render_template('ventas')
+
+@app.route('/pre_ventas')
+def pre_ventas():
+    return render_template('pre_ventas.html')
+
+@app.route('/cashier_functions')
+def cashier_functions():
+    return render_template('cashier_functions.html')
+
+@app.route('/admin_functions')
+def admin_functions():
+    return render_template('admin_functions.html')
 
 @app.route('/mantenimiento')
 def mantenimiento():
     return render_template('mantenimiento.html')
+
+@app.route('/technical_functions')
+def technical_functions():
+    return render_template('technical_functions.html')
+
+@app.route('/admin_functions_maintenance')
+def admin_functions_maintenance():
+    return render_template('admin_functions_maintenance.html')
+
+@app.route('/configure_parameters')
+def configure_parameters():
+    return render_template('technical_functions')
+
+@app.route('/generate_maintenance_report')
+def generate_maintenance_report():
+    return render_template('admin_functions_maintenance')
+
+@app.route('/train_system')
+def train_system():
+    return render_template('admin_functions_maintenance')
 
 @app.route('/emisiones')
 def emisiones():
